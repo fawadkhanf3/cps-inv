89836832c4efe01a02f85bb946d62116ab7e6b41
pettni <pettni@umich.edu>
2014-10-29 14:30:23 -0400

    added new 3d case w/ non-convex cinv set

---
+++ acc_3d_nonconvex/get_dyn1.m
@@ -0,0 +1,39 @@
+function dyn = get_dyn(con, con_lead)
+
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	kappa = con.f1_bar/con.mass;
+	ekt = exp(-kappa*con.dT);
+
+	A = [ ekt 			0  0; 
+	      (ekt-1)/kappa 1  0;
+		  0	 			0  1 ]; 
+	B = (1/(con.f1_bar^2))*[ con.f1_bar*(1-ekt) ;
+			  				 con.mass*(1-ekt) - con.dT*con.f1_bar;
+			  				 0];
+	B_cond_number = max(abs(B));
+	B = B/B_cond_number;
+	
+	E = [0 ; con.dT ; 0];
+	K = [ (con.f0_bar/con.f1_bar)*(ekt-1) ;
+		  (con.f0_bar/(con.f1_bar^2) )*(con.dT*con.f1_bar + con.mass*(ekt-1));
+		  0];
+	A_xu = [0	0	0  1 ;
+			0   0	0  -1;
+			ekt*B_cond_number   0  0  (1-ekt)/con.f1_bar;
+			-ekt*B_cond_number  0  0  -(1-ekt)/con.f1_bar ];
+	b_xu = [B_cond_number*con.umax; 
+	       -B_cond_number*con.umin;
+	       B_cond_number*con.v_max-con.f0_bar*B_cond_number*(ekt-1)/con.f1_bar;
+	       -B_cond_number*con.v_min+con.f0_bar*B_cond_number*(ekt-1)/con.f1_bar];
+	XUset = Polyhedron(A_xu, b_xu);
+
+	XD_plus  = [ 0 0 0 con.vl_max];
+	XD_minus = [ 0 0 0 con.vl_min];
+	
+	dyn = Dyn(A,K,B,XUset,E,XD_plus,XD_minus);
+	dyn.save_constant('B_cond_number', B_cond_number);
+
+end
--- acc_3d_nonconvex/run.m
+++ acc_3d_nonconvex/run.m
@@ -1,3 +1,60 @@
+con = constants;
 
-vec = robust_cinv_3d(4);
-simulate_3d(vec);
+%%%% Create dynamics
+
+dyn = get_dyn1(con);
+pwadyn = get_dyn2(con);
+
+
+%%%% Specify specification sets
+
+%Safe set
+VH = pwadyn.domain;
+S1 = intersect(VH, Polyhedron([con.tau_min -1 0; 0 -1 0], [0; -con.h_min]));
+
+% Goal set
+GA = [con.tau_des -1 0; 1 0 0];
+Gb = [0; con.v_des_max];
+goal = intersect1(S1, Polyhedron('A', GA, 'b', Gb));
+
+
+%%%% Step 1: compute inner cinv set
+
+C0 = dyn.cinv_oi(goal);
+
+
+%%%% Step 2: expand inner cinv set
+
+Cinv_vec = [C0];
+
+while true
+	Ci = intersect1(goal, pwadyn.pre(Cinv_vec(end)));
+	Ci_cvx = merge_in(Ci.Set(2), Ci.Set(1));
+	Ci_cvx = merge_in(Ci_cvx, Ci.Set(3));
+
+	if isEmptySet(mldivide(Ci_cvx, Cinv_vec(end)))
+		break;
+	end
+
+	Cinv_vec = [Cinv_vec; Ci_cvx];
+end
+
+
+%%%% Step 3: expand Cinv_vec in domain
+
+set_mat = {Cinv_vec};
+
+for t=1:6
+	Ct_vec = [];
+
+	for i=1:length(set_mat{end})
+		Ci = pwadyn.pre(set_mat{end}(i));
+		Ci_cvx = merge_in(Ci.Set(2), Ci.Set(1));
+		Ci_cvx = merge_in(Ci_cvx, Ci.Set(3));
+		Ct_vec = [Ct_vec Ci_cvx];
+	end
+
+	set_mat = [set_mat {Ct_vec}];
+end
+
+save('simulation/set_mat.mat', 'set_mat')
