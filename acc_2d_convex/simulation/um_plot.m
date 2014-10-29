b864fa99f288fbfbd442a593f0bc9a40c21a931f
pettni <pettni@umich.edu>
2014-10-29 18:31:34 -0400

    fixed 2d example

---
+++ acc_2d_convex/simulation/um_plot.m
@@ -0,0 +1,43 @@
+lw = 1;
+t60 = find(x_out.time < 50, 1, 'last');
+t80 = find(x_out.time >= 100, 1);
+
+figure(1)
+clf
+subplot(411)
+hold on
+plot(x_out.time, x_out.signals.values(:,1), 'b', 'linewidth', lw)
+plot([0 x_out.time(t60)], [con.v_lead con.v_lead], 'r');
+plot([x_out.time(t80) x_out.time(end)], [con.v_lead con.v_lead], 'r');
+legend('ACC', 'Lead', 'Location', 'NorthOutSide')
+ylabel('$v$')
+xlabel('$t$')
+
+subplot(412)
+hold on
+plot(x_out.time, x_out.signals.values(:,2), 'linewidth', lw)
+plot(get(gca,'xlim'), [3 3], 'g');
+ylabel('$h$')
+xlabel('$t$')
+
+subplot(413)
+hold on
+plot(u_out.time, u_out.signals.values/(9.82*con.mass), 'linewidth', lw)
+xlabel('$t$')
+ylabel('$F_w/mg$')
+plot(get(gca,'xlim'), [con.umax/(con.g*con.mass) con.umax/(con.g*con.mass)], 'g');
+plot(get(gca,'xlim'), [con.umin/(con.g*con.mass) con.umin/(con.g*con.mass)], 'g');
+ylim([1.1*con.umin/(con.g*con.mass) 1.1*con.umax/(con.g*con.mass)])
+
+subplot(414)
+hold on
+plot(u_out.time, max(0, min(3, x_out.signals.values(:,2)./x_out.signals.values(:,1))), 'linewidth', lw)
+plot(get(gca,'xlim'), [con.h_des con.h_des], 'g');
+xlabel('$t$')
+ylabel('$\max(3, h/v)$')
+
+matlab2tikz('simulink.tikz','interpretTickLabelsAsTex',true, ...
+		     'parseStrings',false, 'showInfo', false, ...
+		     'width','\figurewidth', 'height', '\figureheight', ...
+		    'extraAxisOptions', ...
+		    'xmajorgrids=false, ymajorgrids=false, axis x line=bottom, axis y line=left, every axis x label/.style={at={(current axis.south east)},anchor=west}')
---
+++ acc_3d_nonconvex/simulation/plot_sets.m
@@ -0,0 +1,44 @@
+clear all;
+load set_mat
+
+%%%%%%%%%%%%%%%% Plot inner cinv set %%%%%%%%%%%%%%%%
+figure(1); clf;  hold on;
+
+poly200 = Polyhedron('H', [0 1 0 200]);
+plot(intersect1(goal, poly200), 'color', 'blue', 'alpha', 0.1)
+
+plot(intersect1(set_mat{1}(1), poly200), 'color', 'green', 'alpha', 0.3)
+xlabel('$v$')
+ylabel('$h$')
+zlabel('$v_L$')
+view([-28 4])
+
+matlab2tikz('C0.tikz','interpretTickLabelsAsTex',true, ...
+		     'width','\figurewidth', 'height', '\figureheight', ...
+		     'parseStrings',false, 'showInfo', false)
+
+%%%%%%%%%%%%%%%% Plot full cinv set %%%%%%%%%%%%%%%%
+figure(2); clf; hold on
+
+plot(intersect1(S1, poly200), 'color', 'red', 'alpha', 0.1)
+plot(intersect1(goal, poly200), 'color', 'blue', 'alpha', 0.2)
+
+for i=1:2:length(set_mat{1})
+	plot(intersect1(set_mat{1}(i), poly200), 'color', 'green', 'alpha', 0.1)
+end
+for j = 1:4:length(set_mat{end})
+	plot(intersect1(set_mat{end}(j), poly200), 'color', 'green', 'alpha', 0.06, 'linestyle', 'none')
+end
+
+xlabel('$v$')
+ylabel('$h$')
+zlabel('$v_L$')
+view([-28 4])
+text(1, 100, 15, '$C$')
+text(20, 10, 5, '$G$')
+text(28, 10, 5, '$S$')
+text(24, 10, 30, '$S \cap Pre(C)$')
+
+matlab2tikz('C1.tikz','interpretTickLabelsAsTex',true, ...
+		     'width','\figurewidth', 'height', '\figureheight', ...
+		     'parseStrings',false, 'showInfo', false)
