207c1d298cba27a5427e43192ed96609507566c1
pettni <pettni@umich.edu>
2015-02-03 14:44:43 -0500

    added dynamics exports

--- acc_3d_nonconvex_rc/export_data/export_sets.m
+++ acc_3d_nonconvex_rc/export_data/export_sets.m
@@ -1,3 +1,27 @@
+for i=1:pwadyn.num_region
+
+	fileID = fopen(strcat(['dynamics_', num2str(i), '.txt']),'w');
+
+	write_data(pwadyn.reg_list{i}.A, fileID);
+	write_data(pwadyn.reg_list{i}.b, fileID);
+
+	write_data(pwadyn.dyn_list{i}.A, fileID);
+	write_data(pwadyn.dyn_list{i}.K, fileID);
+	write_data(pwadyn.dyn_list{i}.B, fileID);
+	write_data(pwadyn.dyn_list{i}.E, fileID);
+	write_data(pwadyn.dyn_list{i}.Em, fileID);
+
+	write_data(pwadyn.dyn_list{i}.XU_set.A, fileID);
+	write_data(pwadyn.dyn_list{i}.XU_set.b, fileID);
+	write_data(pwadyn.dyn_list{i}.XD_plus, fileID);
+	write_data(pwadyn.dyn_list{i}.XD_minus, fileID);
+
+	fclose(fileID);
+
+end
+
+
+
 for i=1:length(set_mat)
 
 	fileID = fopen(strcat(['sets_', num2str(i), '.txt']),'w');
