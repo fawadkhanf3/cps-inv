0bcc661805791df282db4cbad59c645aea6e2d53
pettni <pettni@umich.edu>
2015-03-05 16:51:53 -0500

    new constants for acc paper

--- acc_3d_nonconvex/constants.m
+++ acc_3d_nonconvex/constants.m
@@ -2,11 +2,11 @@
 function [con] = constants()
 
 	% car dynamics
-	con.mass = 1650;
-	con.f0 = 0.1*con.mass;
+	con.mass = 1370;
+	con.f0 = 0.1;
 	con.f1 = 5;
 	con.f2 = 0.25;
-	con.g = 9.81;
+	con.g = 9.82;
 
 	% linearization
 	con.lin_speed = 10;
@@ -14,20 +14,19 @@ function [con] = constants()
 	con.f1_bar = con.f1 + 2*con.f2*con.lin_speed;
 	con.dT = 0.5;
 
-
 	% domain specs
-	con.v_min = -0.5;
+	con.v_min = -0.1;
 	con.v_max = 35;
 	con.h_min = 0;
-	con.h_max = 300;
+	con.h_max = 200;
 
 	% assumptions
 	con.umin = -0.3*con.mass*con.g;
 	con.umax = 0.2*con.mass*con.g;
 	con.vl_min = 0;
-	con.vl_max = 35;
-	con.al_min = -3; % m/s^2
-	con.al_max = 2;  % m/s^2
+	con.vl_max = 20;
+	con.al_min = -0.97; % m/s^2
+	con.al_max = 0.65;  % m/s^2
 
 	% specifications
 	con.h_min = 3;   % minimum distance
