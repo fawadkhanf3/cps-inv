0d5cc16e2670bee7a2a990af1c4de0ed3eacfd53
pettni <pettni@umich.edu>
2015-06-19 15:31:16 -0400

    asd

---
+++ acc_3d_nonconvex/paper_plots/plot_sets.m
@@ -0,0 +1,27 @@
+load case3
+
+figure; hold on
+plot(C1(1:2:end), 'color', [0 0.5 0], 'alpha', 0.05)
+plot(intersect(C2(1:2:end), Polyhedron('H', [0 1 0 200])), 'color', [0 0 0.5], 'alpha', 0.05)
+xlabel('$v$')
+ylabel('$h$')
+zlabel('$v_L$')
+view([-20 18])
+grid off
+zlim([0 35])
+
+matlab2tikz('tikz/pcis_case3_invset.tikz','interpretTickLabelsAsTex',true, 'width','\figurewidth', 'height', '\figureheight', 'parseStrings',false, 'showInfo', false)
+
+load case4
+
+figure; hold on
+plot(C1(1:2:end), 'color', [0 0.5 0], 'alpha', 0.05)
+plot(intersect(C2(1:2:end), Polyhedron('H', [0 1 0 200])), 'color', [0 0 0.5], 'alpha', 0.05)
+xlabel('$v$')
+ylabel('$h$')
+zlabel('$v_L$')
+view([-20 18])
+grid off
+zlim([0 35])
+
+matlab2tikz('tikz/pcis_case4_invset.tikz','interpretTickLabelsAsTex',true, 'width','\figurewidth', 'height', '\figureheight', 'parseStrings',false, 'showInfo', false)
